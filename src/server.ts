71723544fce66f97ec5b598870585ee7b7192e4d
Alexander Gård <alex.gaard@hotmail.com>
2020-09-30 12:58:33 +0200

    Refaktorering av duplisert kode

--- src/server.ts
+++ src/server.ts
@@ -8,38 +8,38 @@ const publicPath = isAbsolute(env.publicPath)
 	? env.publicPath
 	: join(__dirname, env.publicPath);
 
-const hasContextPath = env.contextPath !== '';
+const contextPath = env.contextPath === ''
+	? '/'
+	: env.contextPath;
 
 const app: express.Application = express();
 
 app.use(helmet());
 
-app.use(hasContextPath ? env.contextPath : '/', express.static(publicPath, {
+app.use(contextPath, express.static(publicPath, {
 	cacheControl: false
 }));
 
-app.get(resolve(env.contextPath, '/internal/isReady'), function (req, res) {
+app.get(resolve(contextPath, '/internal/isReady'), function (req, res) {
 	res.send('');
 });
 
-app.get(resolve(env.contextPath, '/internal/isAlive'), function (req, res) {
+app.get(resolve(contextPath, '/internal/isAlive'), function (req, res) {
 	res.send('');
 });
 
-if (env.redirectOnNotFound) {
-	app.get(resolve(env.contextPath, '/*'), (req, res) => {
-		res.redirect(hasContextPath ? env.contextPath : '/');
-	});
-} else {
-	app.get(resolve(env.contextPath, '/*'), (req, res) => {
+app.get(resolve(contextPath, '/*'), (req, res) => {
+	if (env.redirectOnNotFound) {
+		res.redirect(contextPath);
+	} else {
 		res.sendFile(join(publicPath, 'index.html'));
-	});
-}
+	}
+});
 
 app.listen(env.port, function () {
 	console.log('Starting server with following parameters');
 	console.log(`Public path: ${publicPath}`);
-	console.log(`Context path: ${env.contextPath}`);
+	console.log(`Context path: ${contextPath}`);
 	console.log(`Redirect to index.html for 404: ${env.redirectOnNotFound}`);
 	console.log(`Port: ${env.port}`);
 });
