e9a5383811f45167e6e73f36296721c8f0329873
Alexander Gård <alex.gaard@hotmail.com>
2020-09-30 11:56:26 +0200

    La til mulighet for å configurere diverse parametre

---
+++ src/environment.ts
@@ -0,0 +1,31 @@
+const DEFAULT_PORT = 8080;
+const DEFAULT_PUBLIC_PATH = '/public';
+const DEFAULT_CONTEXT_PATH = '';
+const DEFAULT_REDIRECT_ON_NOT_FOUND = 'true';
+
+class Environment {
+
+	get port(): number {
+		const portFromEnv = process.env.PORT;
+		const port = portFromEnv ? parseInt(portFromEnv) : NaN;
+		return isNaN(port) ? DEFAULT_PORT : port;
+	}
+
+	get publicPath(): string {
+		return process.env.PUBLIC_PATH || DEFAULT_PUBLIC_PATH;
+	}
+
+	get contextPath(): string {
+		return process.env.CONTEXT_PATH || DEFAULT_CONTEXT_PATH;
+	}
+
+	get redirectOnNotFound(): boolean {
+		const redirectOnNotFound = process.env.REDIRECT_ON_NOT_FOUND || DEFAULT_REDIRECT_ON_NOT_FOUND;
+		return redirectOnNotFound === 'true';
+	}
+
+}
+
+const environment = new Environment();
+
+export default environment;
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,44 @@
 import express from 'express';
+import { isAbsolute, join } from 'path';
+import { resolve } from 'url';
+import env from './environment';
+
+const publicPath = isAbsolute(env.publicPath)
+	? env.publicPath
+	: join(__dirname, env.publicPath);
+
+const hasContextPath = env.contextPath !== '';
 
 const app: express.Application = express();
 
-app.get('/', function (req, res) {
-	res.send('Hello World!');
+if (hasContextPath) {
+	app.use(env.contextPath, express.static(publicPath));
+} else {
+	app.use(express.static(publicPath));
+}
+
+app.get(resolve(env.contextPath, '/internal/isReady'), function (req, res) {
+	res.send('');
 });
 
-app.listen(3000, function () {
-	console.log('App is listening on port 3000!');
+app.get(resolve(env.contextPath, '/internal/isAlive'), function (req, res) {
+	res.send('');
+});
+
+if (env.redirectOnNotFound) {
+	app.get(resolve(env.contextPath, '/*'), (req, res) => {
+		res.redirect(hasContextPath ? env.contextPath : '/');
+	});
+} else {
+	app.get(resolve(env.contextPath, '/*'), (req, res) => {
+		res.sendFile(join(publicPath, 'index.html'));
+	});
+}
+
+app.listen(env.port, function () {
+	console.log('Starting server with following parameters');
+	console.log(`Public path: ${publicPath}`);
+	console.log(`Context path: ${env.contextPath}`);
+	console.log(`Redirect to index.html for 404: ${env.redirectOnNotFound}`);
+	console.log(`Port: ${env.port}`);
 });
