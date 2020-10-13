5719bca22fe8502aac1ff8146c83d94051da0b31
Alexander Gård <alex.gaard@hotmail.com>
2020-10-13 11:55:13 +0200

    Tweaking på CSP

--- src/middleware/helmet-middleware.ts
+++ src/middleware/helmet-middleware.ts
@@ -4,12 +4,15 @@ const ALLOWED_DOMAINS = ["*.nav.no", "*.adeo.no"];
 const GOOGLE_ANALYTICS_DOMAIN = "*.google-analytics.com";
 const GOOGLE_TAG_MANAGER_DOMAIN = "*.googletagmanager.com";
 const ACCOUNT_PSPLUGIN_DOMAIN = "account.psplugin.com";
-const STATIC_HOTJAR_DOMAIN = "static.hotjar.com";
+const HOTJAR_DOMAIN = "*.hotjar.com";
+const VARS_HOTJAR_DOMAIN = "vars.hotjar.com";
 
 /**
  * Det hadde vært best å fjerne 'unsafe-inline' fra scriptSrc, men NAV dekoratøren kjører inline scripts som ikke vil fungere uten dette.
  * Denne reglen vil også treffe applikasjoner som bruker create-react-app siden den lager et inline script for å bootstrape appen.
  * Dette kan fikses med å sette "INLINE_RUNTIME_CHUNK=false" i en .env fil.
+ *
+ * unsafe-eval i scriptSrc blir brukt av account.psplugin.com. Hvis vi ikke trenger psplugin så bør dette fjernes.
  */
 
 export function helmetMiddleware() {
@@ -22,10 +25,11 @@ export function helmetMiddleware() {
 				blockAllMixedContent: [],
 				fontSrc: ["'self'", "https:", "data:"].concat(ALLOWED_DOMAINS),
 				frameAncestors: ["'self'"],
+				frameSrc: [VARS_HOTJAR_DOMAIN],
 				objectSrc: ["'none'"],
-				scriptSrc: ["'self'", "'unsafe-inline'"].concat(
+				scriptSrc: ["'self'", "'unsafe-inline'", "'unsafe-eval'"].concat(
 					ALLOWED_DOMAINS, GOOGLE_ANALYTICS_DOMAIN,
-					GOOGLE_TAG_MANAGER_DOMAIN, ACCOUNT_PSPLUGIN_DOMAIN, STATIC_HOTJAR_DOMAIN
+					GOOGLE_TAG_MANAGER_DOMAIN, ACCOUNT_PSPLUGIN_DOMAIN, HOTJAR_DOMAIN
 				),
 				scriptSrcAttr: ["'none'"],
 				styleSrc: ["'self'", "https:", "'unsafe-inline'"].concat(ALLOWED_DOMAINS),
