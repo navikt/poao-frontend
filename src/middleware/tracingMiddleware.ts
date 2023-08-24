4900f5791affbe9d1afee961f166f2451c6fe806
Sigurd Grøneng <sigurdgroneng@gmail.com>
2023-08-24 08:38:14 +0200

    Skip asset warnings (#104)
    * Add path to trace-warning
    
    * Bump versions
    
    * Downgrade url-join to cjs support
    
    * Move health endpoint first
    
    * Skip warning for file-requests

--- src/middleware/tracingMiddleware.ts
+++ src/middleware/tracingMiddleware.ts
@@ -5,13 +5,20 @@ import {logger} from "../utils/logger";
 /* Express seems to lowercase all headers  */
 export const CALL_ID = "nav-call-id"
 export const CONSUMER_ID = "nav-consumer-id"
+
+const assetFileEnding = [".ico", ".map", ".js", ".css", ".html"]
+const assetRequest = (req: Request) => {
+    if (req.path === "/") return true
+    return assetFileEnding
+        .some(fileEnding => req.path.endsWith(fileEnding))
+}
 export const tracingMiddleware = (req: Request, res: Response, next: NextFunction) => {
     const callId = req.headers[CALL_ID]
     if (!callId) {
         req.headers[CALL_ID] = uuidv4()
     }
     const consumerId = req.headers[CONSUMER_ID]
-    if (!consumerId) {
+    if (!consumerId && !assetRequest(req)) {
         logger.warn({
             message: "Request tracing missing: " + JSON.stringify([!callId ? "callId": undefined, !consumerId ? "consumerId": undefined].filter(it => it)),
             callId: req.headers[CALL_ID],
