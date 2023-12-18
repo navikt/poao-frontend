f1a933ab8fb0142b0986c25ee2bfecba1c2e6da4
Mads Lee Giil <mads.lee.giil@nav.no>
2023-12-18 15:04:13 +0100

    Oppgrader http-proxy-middleware

--- src/middleware/proxy-middleware.ts
+++ src/middleware/proxy-middleware.ts
@@ -6,24 +6,26 @@ import {CALL_ID, CONSUMER_ID} from "./tracingMiddleware";
 import {APP_NAME} from "../config/base-config";
 
 export const proxyMiddleware = (proxyContextPath: string, proxy: Proxy): RequestHandler => {
-	return createProxyMiddleware(proxyContextPath, {
+	return createProxyMiddleware({
 		target: proxy.toUrl,
-		logLevel: 'error',
+
 		headers:  {
 			[CONSUMER_ID]: APP_NAME,
 		},
-		logProvider: () => logger,
+		logger: logger,
 		changeOrigin: true,
 		pathRewrite: proxy.preserveFromPath
 			? undefined
 			: { [`^${proxyContextPath}`]: '' },
-		onError: (error, _request, _response) => {
-			logger.error({
-				path: _request.path?.replace(/\d{11}/g, '<fnr>'),
-				stack_trace: error.stack,
-				message: `onError, error=${error.message}`,
-				callId: _request.headers[CALL_ID],
-				consumerId: _request.headers[CONSUMER_ID] });
+		on: {
+			error: (error, _request, _response) => {
+				logger.error({
+					path: _request.path?.replace(/\d{11}/g, '<fnr>'),
+					stack_trace: error.stack,
+					message: `onError, error=${error.message}`,
+					callId: _request.headers[CALL_ID],
+					consumerId: _request.headers[CONSUMER_ID] });
+			}
 		},
 	})
 };
