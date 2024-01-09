f6c1b347dbf99d4411c846ea0623ef2f70aa9087
Sondre Lefsaker <sondre.lefsaker@nav.no>
2024-01-09 17:53:31 +0100

    only log warnings for JSON requests

--- src/middleware/tracingMiddleware.ts
+++ src/middleware/tracingMiddleware.ts
@@ -1,24 +1,19 @@
-import {NextFunction, Request, Response} from "express";
+import { NextFunction, Request, Response } from "express";
 import { v4 as uuidv4 } from 'uuid';
-import {logger} from "../utils/logger";
+import { logger } from "../utils/logger";
 
 /* Express seems to lowercase all headers  */
 export const CALL_ID = "nav-call-id"
 export const CONSUMER_ID = "nav-consumer-id"
 
-const assetFileEnding = [".ico", ".map", ".js", ".css", ".html"]
-const assetRequest = (req: Request) => {
-    if (req.path === "/") return true
-    return assetFileEnding
-        .some(fileEnding => req.path.endsWith(fileEnding))
-}
 export const tracingMiddleware = (req: Request, res: Response, next: NextFunction) => {
     const callId = req.headers[CALL_ID]
     if (!callId) {
         req.headers[CALL_ID] = uuidv4()
     }
+
     const consumerId = req.headers[CONSUMER_ID]
-    if (!consumerId && !assetRequest(req)) {
+   if (!consumerId && isJsonRequest(req)) {
         logger.warn({
             message: "Request tracing missing: " + JSON.stringify([!callId ? "callId": undefined, !consumerId ? "consumerId": undefined].filter(it => it)),
             callId: req.headers[CALL_ID],
@@ -28,5 +23,13 @@ export const tracingMiddleware = (req: Request, res: Response, next: NextFunctio
             referer: req.headers["referer"]?.replace(/\d{11}/g, '<fnr>')
         })
     }
+
     next()
+}
+
+function isJsonRequest(req: Request) {
+    // Since "html" has a higher precedence than "json" in the function call below,
+    // requests that accepts any content (like */*) will be intepreted as wanting
+    // "html" rather than "json"
+    return req.accepts("html", "json") === "json"
 }
