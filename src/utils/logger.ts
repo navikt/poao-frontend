192c5d6b340df7b803b74e875937123eac20f8e1
Hans Petter Simonsen <hans.petter.simonsen@nav.no>
2024-05-07 15:13:20 +0200

    Improve fnr masking to avoid false positives on GUIDs

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -20,7 +20,13 @@ const maskedJsonFormat = format.printf((logEntry) => {
 	})
 
 	// Masker fødselsnummer
-	return jsonLog.replace(/\d{11}/g, '<fnr>')
+	/*
+	Avoid false positives on GUIDs.
+	(?<!\w|-) Negative lookbehing - there is no word character [a-zA-Z0-9] or a minus sign before the 11 digits
+	\d{11}     Eleven digits
+	(?!\w|-)   Negative lookahead - there is no word character or a minus sign after the 11 digits.
+	 */
+	return jsonLog.replace(/(?<!\w|-)\d{11}(?!\w|-)/g, '<fnr>')
 });
 
 export const logger = createLogger({
