d30744b5d4c51f11768f1395b97c1035854d425b
sigurdgroneng <sigurd.groneng@nav.no>
2024-10-04 13:20:03 +0200

    Bump http proxy middleware

--- src/middleware/proxy-middleware.ts
+++ src/middleware/proxy-middleware.ts
@@ -9,23 +9,24 @@ import {normalizePathParams} from "../utils/logger.js";
 export const proxyMiddleware = (proxyContextPath: string, proxy: Proxy): RequestHandler => {
 	return createProxyMiddleware({
 		target: proxy.toUrl,
-		logLevel: 'error',
 		headers: {
 			[CONSUMER_ID]: APP_NAME,
 		},
-		logProvider: () => logger,
+		logger,
 		changeOrigin: true,
 		pathRewrite: proxy.preserveFromPath
 			? undefined
 			: { [`^${proxyContextPath}`]: '' },
-		onError: (error, _request, _response) => {
-			logger.error({
-				path: normalizePathParams(_request.path?.replace(/\d{11}/g, '<fnr>')),
-				stack_trace: error.stack,
-				message: `onError, error=${error.message}`,
-				callId: _request.headers[CALL_ID],
-				consumerId: _request.headers[CONSUMER_ID]
-			});
-		},
+		on: {
+			error: (error, _request, _response) => {
+				logger.error({
+					path: normalizePathParams(_request.path?.replace(/\d{11}/g, '<fnr>')),
+					stack_trace: error.stack,
+					message: `onError, error=${error.message}`,
+					callId: _request.headers[CALL_ID],
+					consumerId: _request.headers[CONSUMER_ID]
+				});
+			}
+		}
 	})
 };
