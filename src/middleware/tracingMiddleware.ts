9b752cf661ea1479b5d4a7e78d6b3e5a6f3bed30
sigurdgroneng <sigurd.groneng@nav.no>
2025-09-23 08:08:47 +0200

    Remove unnecessary logging

--- src/middleware/tracingMiddleware.ts
+++ src/middleware/tracingMiddleware.ts
@@ -1,6 +1,5 @@
 import { NextFunction, Request, Response } from "express";
 import { v4 as uuidv4 } from 'uuid';
-import { logger } from "../utils/logger.js";
 
 /* Express seems to lowercase all headers  */
 export const CALL_ID = "nav-call-id"
@@ -11,25 +10,6 @@ export const tracingMiddleware = (req: Request, _res: Response, next: NextFuncti
     if (!callId) {
         req.headers[CALL_ID] = uuidv4()
     }
-
-    const consumerId = req.headers[CONSUMER_ID]
-    if (!consumerId && isJsonRequest(req)) {
-        logger.warn({
-            message: "Request tracing missing: " + JSON.stringify([!callId ? "callId" : undefined, !consumerId ? "consumerId" : undefined].filter(it => it)),
-            callId: req.headers[CALL_ID],
-            consumerId: req.headers[CONSUMER_ID],
-            path: req.path?.replace(/\d{11}/g, '<fnr>'),
-            origin: req.headers["origin"],
-            referer: req.headers["referer"]?.replace(/\d{11}/g, '<fnr>')
-        })
-    }
-
     next()
 }
 
-function isJsonRequest(req: Request) {
-    // Since "html" has a higher precedence than "json" in the function call below,
-    // requests that accepts any content (like */*) will be intepreted as wanting
-    // "html" rather than "json"
-    return req.accepts("html", "json") === "json"
-}
