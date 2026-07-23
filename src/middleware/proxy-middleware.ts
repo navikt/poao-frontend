18246e729478cb78db43d34430b1db1e337205ea
Sigurd Grøneng <sigurdgroneng@gmail.com>
2026-07-23 10:48:20 +0200

    passthrough consumer id (#393)
    * Add tests for proxying before updating proxy middleware
    
    * Format tests because ai broke formatting
    
    * Forbedre bruk av routeUrl slik at det blir likere som det var
    
    * Ikke overskriv nav-consumer-id header på proxy requester

--- src/middleware/proxy-middleware.ts
+++ src/middleware/proxy-middleware.ts
@@ -3,15 +3,11 @@ import { createProxyMiddleware } from 'http-proxy-middleware';
 import { logger } from '../utils/logger.js';
 import { Proxy } from '../config/proxy-config.js';
 import { CALL_ID, CONSUMER_ID } from "./tracingMiddleware.js";
-import { APP_NAME } from "../config/base-config.js";
 import { normalizePathParams } from "../utils/logger.js";
 
 export const proxyMiddleware = (proxyContextPath: string, proxy: Proxy): RequestHandler => {
 	return createProxyMiddleware({
 		target: `${proxy.toUrl}${proxy.preserveFromPath ? proxyContextPath : ''}`,
-		headers: {
-			[CONSUMER_ID]: APP_NAME,
-		},
 		// logger, // Enable this to log each proxied call
 		changeOrigin: true,
 		on: {
